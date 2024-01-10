7fef49f601ae5e676d33da67c24d2aaf8180c7ac
Karilaw <lawrencekariithi6@gmail.com>
2024-01-10 16:03:45 +0000

    second commit to test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,20 +5,15 @@
 def canUnlockAll(boxes):
     """ Create a set to store the keys
     """
-
     keys = {0}
-
     while True:
         new_keys = set()
         for key in keys:
             for box in boxes[key]:
                 if box < len(boxes):
                     new_keys.add(box)
-
-
         if not new_keys - keys:
             break
-
         keys.update(new_keys)
 
     return len(keys) == len(boxes)
